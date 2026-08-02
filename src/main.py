0695cda398c26c4427d8d9df1a2182aa426db878
Bittesh Bhattacharya <bitteshbhattacharya@gmail.com>
2026-08-03 04:35:39 +0530

    bittesh

--- src/main.py
+++ src/main.py
@@ -14,9 +14,10 @@ questions =[
 def run_test(questions):
     score=0
     for question in questions:
-        answer=input(question.prompt + "\n" +"Your answer is : ")
+        answer=input(question.prompt + "\n" +"Your answer is kpratyush: ")
         if answer == question.answer:
             score+= 1
     print ("You got " + str(score) + "/" + str(len(questions)) + " correct")
+    print("hallo")
 
 run_test(questions) 
